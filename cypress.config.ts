e7646d75495b77f3b6da7840d9746357765761f2
Brant Burnett <bburnett@centeredgesoftware.com>
2024-02-04 21:47:57 +0000

    Upgrade Cypress

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  videosFolder: 'cypress/videos',
+  screenshotsFolder: 'cypress/screenshots',
+  fixturesFolder: 'cypress/fixtures',
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+  },
+})
